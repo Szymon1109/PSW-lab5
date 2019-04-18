802e51ac95ad19d6a4d2bd1fba2f27a055e74f38
Szymon1109 <szymonbetlewski@wp.pl>
2019-04-18 11:02:05 +0200

    Good update UserDAO.java

--- Rejestration/src/dao/UserDAO.java
+++ Rejestration/src/dao/UserDAO.java
@@ -1,8 +1,6 @@
 package dao;
 
-import model.Event;
 import model.User;
-
 import java.util.List;
 
 public interface UserDAO {
@@ -11,18 +9,9 @@ public interface UserDAO {
     Boolean findOne(String login);
 
     String whoIs(String login);
-
     List findAllUsers();
-    List findAllEvents();
 
     void save(User user);
-    void save(Event event);
-
     void changePassword(User user, String newPassword);
-    void update(Event event);
-
     void delete(User user);
-    void delete(Event event);
-
-    void confirm(User user, Event event);
 }
