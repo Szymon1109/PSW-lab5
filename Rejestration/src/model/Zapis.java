44bb65e0d3b3787b9dcee0aa862e59bcdab29c05
Szymon1109 <szymonbetlewski@wp.pl>
2019-04-22 18:28:46 +0200

    Add two new poles in model Zapis.java

--- Rejestration/src/model/Zapis.java
+++ Rejestration/src/model/Zapis.java
@@ -4,18 +4,24 @@ public class Zapis {
     private Integer id;
     private Integer idUzytkownika;
     private Integer idWydarzenia;
+    private String typUczestnictwa;
+    private String wyzywienie;
     private String zgoda;
 
-    public Zapis(Integer id, Integer id_uzytkownika, Integer id_wydarzenia, String zgoda) {
+    public Zapis(Integer id, Integer idUzytkownika, Integer idWydarzenia, String typUczestnictwa, String wyzywienie, String zgoda) {
         this.id = id;
-        this.idUzytkownika = id_uzytkownika;
-        this.idWydarzenia = id_wydarzenia;
+        this.idUzytkownika = idUzytkownika;
+        this.idWydarzenia = idWydarzenia;
+        this.typUczestnictwa = typUczestnictwa;
+        this.wyzywienie = wyzywienie;
         this.zgoda = zgoda;
     }
 
-    public Zapis(Integer id_uzytkownika, Integer id_wydarzenia, String zgoda) {
-        this.idUzytkownika = id_uzytkownika;
-        this.idWydarzenia = id_wydarzenia;
+    public Zapis(Integer idUzytkownika, Integer idWydarzenia, String typUczestnictwa, String wyzywienie, String zgoda) {
+        this.idUzytkownika = idUzytkownika;
+        this.idWydarzenia = idWydarzenia;
+        this.typUczestnictwa = typUczestnictwa;
+        this.wyzywienie = wyzywienie;
         this.zgoda = zgoda;
     }
 
@@ -31,16 +37,32 @@ public class Zapis {
         return idUzytkownika;
     }
 
-    public void setIdUzytkownika(Integer id_uzytkownika) {
-        this.idUzytkownika = id_uzytkownika;
+    public void setIdUzytkownika(Integer idUzytkownika) {
+        this.idUzytkownika = idUzytkownika;
     }
 
     public Integer getIdWydarzenia() {
         return idWydarzenia;
     }
 
-    public void setIdWydarzenia(Integer id_wydarzenia) {
-        this.idWydarzenia = id_wydarzenia;
+    public void setIdWydarzenia(Integer idWydarzenia) {
+        this.idWydarzenia = idWydarzenia;
+    }
+
+    public String getTypUczestnictwa() {
+        return typUczestnictwa;
+    }
+
+    public void setTypUczestnictwa(String typUczestnictwa) {
+        this.typUczestnictwa = typUczestnictwa;
+    }
+
+    public String getWyzywienie() {
+        return wyzywienie;
+    }
+
+    public void setWyzywienie(String wyzywienie) {
+        this.wyzywienie = wyzywienie;
     }
 
     public String getZgoda() {
